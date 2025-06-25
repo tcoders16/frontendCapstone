88e2b264eea7c2b593311df79005013d1a1b65b8
tcoders16 <tcoders08164002@gmail.com>
2025-06-25 16:11:29 -0400

    RecognitionEngine

--- App.tsx
+++ App.tsx
@@ -4,11 +4,11 @@ import './global.css';
 import UploadImageScreen from './pages/UploadImageScreen';
 import { View,Text } from 'react-native';
 import { ErrorBoundary } from 'react-error-boundary';
-
+import SpecificationCard from './pages/RecognitionEngine'
 export default function App() {
   return (
     <ErrorBoundary fallback={<Text>Something broke</Text>}>
-      <UploadImageScreen />
+      <SpecificationCard title={'Glasses'} description={'A golden frame round glass '} type={'Glass'} specification={'Scratch on right side '} />
     </ErrorBoundary>
   );
 }
